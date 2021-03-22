7b7d3644d14712f58b3edafceebb353f1b951202
spoorn <michael.trinh1996@gmail.com>
2021-03-22 14:20:48 -0700

    Bump to 1.1.1, integrate with Mod menu

---
+++ src/main/java/org/spoorn/climbladdersfast/config/ClimbLaddersFastModMenuApiImpl.java
@@ -0,0 +1,16 @@
+package org.spoorn.climbladdersfast.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+/**
+ * Mod Menu config screen.
+ */
+public class ClimbLaddersFastModMenuApiImpl implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
